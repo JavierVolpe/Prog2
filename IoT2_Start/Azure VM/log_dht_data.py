902ae5ccc989e2b377d2608d150b4109d01eeca0
JavierVolpe <javo0001@stud.kea.dk>
2024-03-03 14:25:04 +0100

    Extra comments

--- IoT2_Start/Azure VM/log_dht_data.py
+++ IoT2_Start/Azure VM/log_dht_data.py
@@ -1,28 +1,29 @@
-# Description: This script subscribes to the MQTT broker and logs the temperature and humidity data to a SQLite database
-
-
+# Import necessary libraries
 import sqlite3
 from datetime import datetime
 from time import sleep
-
 import paho.mqtt.subscribe as subscribe
 
+# Define the path to the SQLite database
 db_path = "db/sensor_data.db"
 
-
 def create_table():
+    """
+    This function creates a new table in the SQLite database if it doesn't already exist.
+    """
+    # Connect to the SQLite database
     conn = sqlite3.connect(db_path)
     curs = conn.cursor()
 
     # Check if the table already exists
-    curs.execute(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='stue' LIMIT 1;"
-    )
+    curs.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='stue' LIMIT 1;")
     if curs.fetchone():
         print("Table already exists")
     else:
+        # SQL query to create a new table
         query = """CREATE TABLE stue (datetime TEXT, temperature REAL, humidity REAL)"""
         try:
+            # Execute the SQL query
             curs.execute(query)
             conn.commit()
             print("Table created successfully")
@@ -31,19 +32,24 @@ def create_table():
             print("Failed to connect to the database")
             conn.rollback()
         finally:
+            # Close the cursor
             curs.close()
 
-
 def log_stue_data(client, userdata, message):
+    """
+    This function logs the temperature and humidity data to the SQLite database.
 
+    Args:
+        client: The MQTT client instance for this callback
+        userdata: The private user data as set in Client() or userdata_set()
+        message: An instance of MQTTMessage. This is a class with members topic, payload, qos, retain.
+    """
+    # SQL query to insert data into the 'stue' table
     query = """INSERT INTO stue (datetime, temperature, humidity) VALUES (?, ?, ?)"""
- 
-    # Example of payload: b'2024-03-02 16:52:54|22.0|41.0' 
-    msg_str = message.payload.decode("utf-8")
 
-    time = msg_str.split("|")[0]
-    temperature = msg_str.split("|")[1]
-    humidity = msg_str.split("|")[2]
+    # Decode the message payload and split it into components
+    msg_str = message.payload.decode("utf-8")
+    time, temperature, humidity = msg_str.split("|")
     data = (time, temperature, humidity)
     print(f"Data to be inserted: {data}")
 
@@ -51,30 +57,31 @@ def log_stue_data(client, userdata, message):
     try:
         conn = sqlite3.connect(db_path)
         curs = conn.cursor()
+        # Execute the SQL query
         curs.execute(query, data)
         conn.commit()
-        # print(f"Data inserted successfully: {data}")
     except Exception as e:
         print("Error:", e)
         print("Failed to connect to the database")
         conn.rollback()
-        # conn.close()
     finally:
+        # Close the cursor
         curs.close()
     sleep(0.5)
 
 def start_logging():
+    """
+    This function starts the logging process.
+    """
     try:
+        # Create the table if it doesn't exist
         create_table()
+        # Subscribe to the MQTT topic and log the data
         subscribe.callback(log_stue_data, "stue/data", hostname="52.178.210.165")
-
-
     except Exception as e:
         print("Error:", e)
-
     except KeyboardInterrupt:
         print("Program stopped by the user")
 
-
-
+# Start the logging process
 start_logging()
